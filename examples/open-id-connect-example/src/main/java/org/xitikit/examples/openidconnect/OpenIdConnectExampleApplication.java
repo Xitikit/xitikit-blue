577311ea74fb380e1a32cc49cf5d523978f51a0d
Keith Hoopes <xitikit.org@gmail.com>
2017-04-14 12:50:43 -0600

    Added gradle files missing from last commit.

---
+++ examples/open-id-connect-example/src/main/java/org/xitikit/examples/openidconnect/OpenIdConnectExampleApplication.java
@@ -0,0 +1,12 @@
+package org.xitikit.examples.openidconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenIdConnectExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenIdConnectExampleApplication.class, args);
+	}
+}
