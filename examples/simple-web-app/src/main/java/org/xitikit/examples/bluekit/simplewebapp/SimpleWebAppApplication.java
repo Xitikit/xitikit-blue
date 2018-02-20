9b3a5e84b88ee44583b599dfd160a81ccdbdcefc
Keith Hoopes <keith.hoopes@xitikit.org>
2018-02-20 02:38:35 -0700

    New Example: simple-web-app

---
+++ examples/simple-web-app/src/main/java/org/xitikit/examples/bluekit/simplewebapp/SimpleWebAppApplication.java
@@ -0,0 +1,16 @@
+package org.xitikit.examples.bluekit.simplewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebAppApplication{
+
+
+
+
+    public static void main(String[] args){
+
+        SpringApplication.run(SimpleWebAppApplication.class, args);
+    }
+}
